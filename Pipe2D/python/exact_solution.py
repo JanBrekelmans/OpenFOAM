1a40a8af88930bfe7542775746384d3ff20430b5
Jan Brekelmans <j.j.w.c.brekelmans@gmail.com>
2022-09-25 17:51:29 +0200

    Added sampling to Pipe2d case.

---
+++ Pipe2D/python/exact_solution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 17:13:38 2022
+
+@author: jjwcb
+"""
+
+import numpy as np
+
+diameter = 0.1
+viscosity = 0.001
+
+num_points = 21
+
+u_sol = np.zeros(21)
